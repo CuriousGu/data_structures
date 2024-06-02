b2fdcf7713d9577cbedf0a89b3637dbb88d8374e
CuriousGu <gustavo_ortega@usp.br>
2024-06-02 05:07:17 -0300

    binary tree - python

--- trees/binary_tree/binary_tree.py
+++ trees/binary_tree/binary_tree.py
@@ -17,21 +17,62 @@ class BinaryTree:
         return self.__size
 
     def append(self, item: int) -> None:
+        if not self.root:
+            self.root = BinaryTree.Node(item)
+            self.__size += 1
+            return
+
         pointer = self.root
 
         while pointer:
-            if pointer.value >= item:
+            if pointer.value > item and pointer._left:
                 pointer = pointer._left
-            else:
+            elif pointer.value < item and pointer._right:
                 pointer = pointer._right
+            else:
+                break
 
+        if pointer.value < item:
+            pointer._right = BinaryTree.Node(item)
+        else:
+            pointer._left = BinaryTree.Node(item)
         self.__size += 1
 
-        if item > pointer.value:
-            pointer.right = BinaryTree.Node(item)
-        else:
-            pointer.left = BinaryTree.Node(item)
+    def remove(self, item: int) -> None:
+        pointer = self.root
 
-a = BinaryTree()
-a.append(2)
-print(len(a))
+        # finding node
+        if not pointer.value:
+            raise ValueError("Empty Tree")
+        elif pointer.value == item:
+            pass
+        else:
+            while pointer:
+                if pointer.value < item:
+                    parent = pointer
+                    pointer = pointer._right
+                elif pointer.value > item:
+                    parent = pointer
+                    pointer = pointer._left
+                else:
+                    break
+
+        if pointer._right:
+            replace_value = pointer._right
+            while replace_value._left:
+                parent = replace_value
+                replace_value = replace_value._left
+            pointer.value = replace_value.value
+            parent._left = None
+        elif pointer._left:
+            replace_value = pointer._left
+            while replace_value._right:
+                replace_value = replace_value._right
+            pointer.value = replace_value.value
+            parent._right = None
+        else:
+            if parent._left.value == item:
+                parent._left = None
+            else:
+                parent._right = None
+        self.__size -= 1
